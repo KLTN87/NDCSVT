6d337e50003be97a2cde3dbc62a4591a6a846295
Moleys <34809211+Moleys@users.noreply.github.com>
2021-10-29 23:12:54 +0700

    test chia ảnh

---
+++ NDCSVT/NDCSVT/frmSeparate.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Grabcut
+{
+    partial class frmTesst
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(518, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(375, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 128);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // frmTesst
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 296);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmTesst";
+            this.Text = "frmTesst";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ NDCSVT/NDCSVT/frmSeparate.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.Features2D;
+using Emgu.CV.XFeatures2D;
+using Emgu.CV.CvEnum;
+
+
+namespace Grabcut
+{
+    public partial class frmTesst : Form
+    {
+        public frmTesst()
+        {
+            InitializeComponent();
+        }
+        string inputfile;
+        Image<Bgr, byte> imgInput;
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                OpenFileDialog opf = new OpenFileDialog();
+                opf.Title = "Select images";
+                opf.Filter = "Image Files | *.jpg; *.jpeg; *.png";
+
+                if (opf.ShowDialog() == DialogResult.OK)
+                {
+                    inputfile = opf.FileName;
+                    imgInput = new Image<Bgr, byte>(inputfile);
+                }
+                pictureBox1.Image = imgInput.ToBitmap();
+            }
+            catch
+            {
+                //không mở ảnh
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            separateImages(imgInput);
+            int countA = 0;
+
+            foreach(var tr in listImages)
+            {
+                countA++;
+
+                int num = rnd.Next(1, 10000);
+
+                CvInvoke.Imshow("image " + num.ToString(), tr);
+
+
+            }
+            MessageBox.Show(countA.ToString());
+
+
+
+
+        }
+
+
+        List<Image<Bgr, Byte>> listImages = new List<Image<Bgr, byte>>();
+
+        public void separateImages(Image<Bgr, Byte> img)
+        {
+
+
+            while (true)
+            {
+
+
+                try
+                {
+                    var cropNgoai = frmGetFeature.cropNoUseBlackAreaImg(img);
+                    var crop1 = cropOneNotBlack(cropNgoai);
+
+                    listImages.Add(crop1[0]);
+                    img = crop1[1];
+
+                }
+                catch(Exception)
+                {
+                    return;
+                }
+            }
+
+
+        }
+
+
+
+        public static List<Image<Bgr, Byte>> cropOneNotBlack(Image<Bgr, Byte> img)
+        {
+
+            List<Image<Bgr, Byte>> tempImg = new List<Image<Bgr, byte>>();
+
+            int toaDoX = getBlackAreaX(img);
+
+            try
+            {
+                Mat ROI = new Mat(img.Mat, new Range(0, img.Height), new Range(0, toaDoX)).Clone();
+                Image<Bgr, Byte> temp0 = ROI.ToImage<Bgr, Byte>();
+
+                Mat ROI1 = new Mat(img.Mat, new Range(0, img.Height), new Range(toaDoX, img.Width)).Clone();
+                Image<Bgr, Byte> temp1 = ROI1.ToImage<Bgr, Byte>();
+                tempImg.Add(temp0);
+                tempImg.Add(temp1);
+
+
+            }
+            catch { }
+
+            return tempImg;
+
+        }
+
+
+        public static int getBlackAreaX(Image<Bgr, Byte> imgBlack)
+        {
+            Bitmap img = imgBlack.AsBitmap();
+            Color pixelColor;
+
+            try
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    int count = 0;
+                    for (int y = 0; y < img.Height; y++)
+                    {
+                        pixelColor = img.GetPixel(x, y);
+                        if (pixelColor.R == 0 && pixelColor.G == 0 && pixelColor.B == 0)
+                        {
+                            count++;
+                        }
+
+                        if (count == img.Height)
+                        {
+
+                            return x;
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return 0;
+
+        }
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = @"PNG|*.png" })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var cropNgoai = frmGetFeature.cropNoUseBlackAreaImg(imgInput);
+
+
+
+                    cropNgoai.Save(saveFileDialog.FileName);
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+}
