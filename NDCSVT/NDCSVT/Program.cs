f0e21c5e619f7f02449c4b89730ad03c5e4bd2a6
gendeathz <lehiennhan2000@gmail.com>
2021-11-05 18:11:35 +0700

    Revert "gd"
    This reverts commit 54c54789fe352ae108925641ccced3b4a46f8380, reversing
    changes made to 90d3dbcdce9a1ba8606913d7fd86d12b8fc7f868.

--- NDCSVT/NDCSVT/Program.cs
+++ NDCSVT/NDCSVT/Program.cs
@@ -14,14 +14,10 @@ namespace Grabcut
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 <<<<<<< HEAD
-<<<<<<< HEAD
             Application.Run(new frmMenu());
 =======
             Application.Run(new frmPredictCNN());
 >>>>>>> 2a04506cf976f2622eee29d6cc91152f532c13ae
-=======
-            Application.Run(new frmTesst());
->>>>>>> 3d547799de546be9efdb19cccf79d703e837a8e6
         }
     }
 }
